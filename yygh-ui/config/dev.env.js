a7544a75867b06f542d3de4ac9b3148fd1e8e106
OvenBryant <3063899248@qq.com>
2023-08-19 13:02:39 +0800

    first commit

--- yygh-ui/config/dev.env.js
+++ yygh-ui/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8202"',   // 必须要加引号
+  BASE_API: '"http://localhost:9001"',   // 必须要加引号
 })
